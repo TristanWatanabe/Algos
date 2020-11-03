2a760baa9b746a1d38231548d6b7cc7530c37749
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-03 18:29:58 -0500

    Solution Added

--- MinimumSwapsToGroupAll1sTogether.js
+++ MinimumSwapsToGroupAll1sTogether.js
@@ -4,7 +4,7 @@
  *      maxOnes - max number of 1s in a valid sliding window
  *      windowTotal - current number of 1s in current sliding window
  *      start and end - ptrs to keep track of sliding window
- * Loop starting from index 0 to array length
+ * While loop
  *      add to windowTotal data at index end
  *      if window is full
  *          - compare maxOnes to windowTotal and set maxOnes equal to whichever is higher
@@ -13,13 +13,29 @@
  *      increment end ptr
  * Return totalOnes - maxOnes
  */
-const minSwaps = data => {
-    
-}
+const minSwaps = (data) => {
+  const totalOnes = data.filter((num) => num === 1).length;
+  if (totalOnes === 1) return 0;
+  let maxOnes = 0,
+    windowOnes = 0;
+  let start = 0,
+    end = 0;
+  while (end < data.length) {
+    windowOnes += data[end];
+    //reached the max the sliding window can be
+    if (end - start + 1 === totalOnes) {
+      //find the most number of 1s in a max sliding window
+      maxOnes = Math.max(maxOnes, windowOnes);
+      //remove start value from sliding window total
+      windowOnes -= data[start++];
+    }
+    end++;
+  }
+  //difference is the number of swaps needed since maxOnes is already the MOST number of 1s that can be in a sliding window
+  return totalOnes - maxOnes;
+};
 
-
-
- /**
+/**
  * N - number of elements in input array
  * Time Complexity - O(N)
  * Space Complexity - O(1)
