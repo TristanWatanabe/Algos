87bd7186ef7e8cf0fbdd25490be2b9f6e0dc5304
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-03 17:55:08 -0500

    Breakdown Added

---
+++ MinimumSwapsToGroupAll1sTogether.js
@@ -0,0 +1,24 @@
+/**
+ * Variables:
+ *      totalOnes - total number of 1s in input array
+ *      maxOnes - max number of 1s in a valid sliding window
+ *      windowTotal - current number of 1s in current sliding window
+ *      start and end - ptrs to keep track of sliding window
+ * Loop starting from index 0 to array length
+ *      add to windowTotal data at index end
+ *      if window is full
+ *          - compare maxOnes to windowTotal and set maxOnes equal to whichever is higher
+ *          - subtract data at index start from windowTotal
+ *          - increment start ptr
+ *      increment end ptr
+ * Return totalOnes - maxOnes
+ */
+const minSwaps = data => {
+
+}
+
+
+
+ /**
+ * 
+ */
