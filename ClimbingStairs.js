e6c84908d99f305b43d93d88dff24bfbc0cb5df2
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-05 17:32:58 -0500

    Solution Added

--- ClimbingStairs.js
+++ ClimbingStairs.js
@@ -9,3 +9,11 @@
  * Return dp at index input n
  *
  */
+
+const climbStairs = (n) => {
+  let dp = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    dp.push(dp[i - 1] + dp[i - 2]);
+  }
+  return dp[n];
+};
