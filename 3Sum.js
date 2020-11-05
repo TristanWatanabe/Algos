d0f66236d84643069ac5ca8152e4b73b628764fa
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-04 22:37:08 -0500

    Breakdown Added

--- 3Sum.js
+++ 3Sum.js
@@ -1,5 +1,22 @@
 /**
+ * Variables:
+ *      triples - array with all triplets that equal to zero
  *
+ * Sort array in ascending order
+ * Loop starting from last index of input array to 0
+ *      - if curr element is less than 0, break loop because we wont be able to find a triplet that would add up to 0
+ *      - if curr element is equal to previous element, skip element in order to avoid duplicates
+ *      - left pointer starting at 0 and right pointer starting from index - 1
+ * Loop while left is less than right
+ *      - variable sum to hold total of curr element, and elements at index left and right
+ *      - if sum is less than zero OR the current element at index left is equal to previous element
+ *          - increment left pointer
+ *      - else if sum is greater than zero OR the current element at index right is equal to previous element
+ *          - decrement right pointer
+ *      - else if sum is equal to zero
+ *          - push to triplets array curr element, and elements at index left and right
+ *          - increment left pointer and decrement right pointer
+ * Return triplets
  */
 
 const threeSum = (nums) => {
