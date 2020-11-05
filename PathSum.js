f80b2e56c0ca460c5dd4f2f1aac9d1e90f2d1a39
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-05 18:03:42 -0500

    Solution Added

--- PathSum.js
+++ PathSum.js
@@ -6,3 +6,13 @@
  * recurse on node.left and node.right
  * return if recursed node.left or node.right finds a path equal to sum
  */
+
+const hasPathSum = (root, sum, currSum = 0) => {
+  if (!root) return false;
+  currSum += root.val;
+  //if node is a leaf
+  if (!root.left && !root.right) return currSum === sum;
+  return (
+    hasPathSum(root.left, sum, currSum) || hasPathSum(root.right, sum, currSum)
+  );
+};
