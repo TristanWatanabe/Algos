aea9e610f17fda50f63d84b06b5a2fde69860c5d
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-06 11:57:51 -0500

    Breakdown Added

--- GenerateParenthesis.js
+++ GenerateParenthesis.js
@@ -1,5 +1,15 @@
 /**
+ * MAIN: n
+ *  Variables:
+ *      result - array to hold all possible string combinations of n*2 length
+ *  Call backtrack function to generate all combinations
+ *  Return result
  *
+ * BACKTRACK: currString, numOfOpenParens, numOfCloseParens, n, reference to result array
+ *  Base Case: if valid combination, push currString to result
+ *  Two recursive cases to form a valid combination that equals to n*2 length:
+ *      If open is less than n, backtrack with currString + "(" and increment open
+ *      If close is less than open, backtrack with currString + ")" and increment close
  */
 
 const generateParenthesis = (n) => {
@@ -13,7 +23,3 @@ const backtrack = (str, open, close, n, result) => {
   if (open < n) backtrack(str + "(", open + 1, close, n, result);
   if (close < open) backtrack(str + ")", open, close + 1, n, result);
 };
-
-/**
- *
- */
