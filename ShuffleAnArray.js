7544a635ad846442071d83b08aec08bd5a774ff1
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-07 20:57:33 -0500

    Solution Added

--- ShuffleAnArray.js
+++ ShuffleAnArray.js
@@ -14,3 +14,26 @@
  *      - Return shuffled array
  *
  */
+
+class Solution {
+  constructor(nums) {
+    this.original = nums;
+    this.shuffled = [...nums];
+  }
+
+  reset() {
+    this.shuffled = [...this.original];
+    return this.shuffled;
+  }
+
+  shuffle() {
+    const length = this.shuffled.length;
+    for (let i = 0; i < length; i++) {
+      let randomIdx = Math.floor(Math.random() * length + 0);
+      let temp = this.shuffled[randomIdx];
+      this.shuffled[randomIdx] = this.shuffled[i];
+      this.shuffled[i] = temp;
+    }
+    return this.shuffled;
+  }
+}
