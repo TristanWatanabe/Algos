df9ecca939f207011684513d9d89c6ff51aab617
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-07 20:54:15 -0500

    Breakdown Added

---
+++ ShuffleAnArray.js
@@ -0,0 +1,16 @@
+/**
+ * Constructor: array
+ *      original - copy of original input array
+ *      shuffled - shuffled version of input array
+ *
+ * Reset:
+ *      - set shuffled back to original array
+ *      - return shuffled
+ * Shuffle:
+ *      - Loop through entire shuffled array
+ *          - find a random index and have temp variable hold the value for the element at index random
+ *          - set element at random index to equal element at index i
+ *          - set element at index i to equal temp variable
+ *      - Return shuffled array
+ *
+ */
