716a3631a80effb522dc896dcb6d6936b989127b
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-09 21:55:07 -0500

    Breakdown Added

---
+++ MeetingRooms.js
@@ -0,0 +1,7 @@
+/**
+ * Sort Array in ascending order
+ * Loop starting from 1 to end of input
+ *  - If start time of current meeting is less than end time of previous meeting
+ *      - return false
+ * Return true
+ */
