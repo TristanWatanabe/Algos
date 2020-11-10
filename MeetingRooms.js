de8c688a62e80a4ab54aa2712a3261b05a21293d
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-09 21:57:51 -0500

    Solution Added

--- MeetingRooms.js
+++ MeetingRooms.js
@@ -5,3 +5,11 @@
  *      - return false
  * Return true
  */
+
+const canAttendMeetings = (meetings) => {
+  meetings.sort((a, b) => a[0] - b[0]);
+  for (let i = 1; i < meetings.length; i++) {
+    if (meetings[i][0] < meetings[i - 1][1]) return false;
+  }
+  return true;
+};
