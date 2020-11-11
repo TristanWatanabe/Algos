9bcbf848ea92e85659083be0123d3eb06eb38aeb
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-11 17:07:03 -0500

    Breakdown Added

---
+++ RemoveAllAdjacentDuplicatesInString.js
@@ -0,0 +1,9 @@
+/**
+ * Variables:
+ *      stack - stack to hold the characters in input that have no duplicates
+ * Loop until end of input string
+ *      - if character at top of stack is equal to current character
+ *          - pop from stack since that means we've encountered a duplicate
+ *      - else: push current character to stack
+ * Return all remaining characters in stack in String format
+ */
