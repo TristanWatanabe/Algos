7a126a97a704340c360c21f8ed1997db21d7f8e7
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-11 16:48:07 -0500

    Solution Added

--- AddTwoNumbersII.js
+++ AddTwoNumbersII.js
@@ -21,3 +21,44 @@
  *      - move curr pointer to next
  * Return result.next
  */
+
+const addTwoNumbers = (l1, l2) => {
+  let s1 = [],
+    s2 = [],
+    sumStack = [];
+  let p1 = l1,
+    p2 = l2;
+  let result = new ListNode();
+  let carry = 0;
+  let curr = result;
+
+  while (p1 || p2) {
+    if (p1) {
+      s1.push(p1.val);
+      p1 = p1.next;
+    }
+    if (p2) {
+      s2.push(p2.val);
+      p2 = p2.next;
+    }
+  }
+  while (s1.length || s2.length || carry) {
+    let val1 = s1.length ? s1.pop() : 0;
+    let val2 = s2.length ? s2.pop() : 0;
+    let sum = val1 + val2 + carry;
+    if (sum > 9) {
+      sum %= 10;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    sumStack.push(sum);
+  }
+  while (sumStack.length) {
+    let sumNode = new ListNode(sumStack.pop());
+    curr.next = sumNode;
+    curr = curr.next;
+  }
+
+  return result.next;
+};
