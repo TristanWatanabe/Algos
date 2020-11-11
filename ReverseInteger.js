9187bab566925da2fa0bb6d81d7db5fe16550610
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-10 23:25:36 -0500

    Breakdown Addded

---
+++ ReverseInteger.js
@@ -0,0 +1,13 @@
+/**
+ * Variable:
+ *      x - string version of input
+ *      result - holds result value
+ * If first character is a negative sign
+ *      - set result to equal the reverse of x without excluding the negative sign
+ *      - Typecast result to a number
+ * Else
+ *      - set result to equal the reverse of x
+ *      - Typecast result to a number
+ * If result doesnt overflow, return result
+ * Else return 0
+ */
