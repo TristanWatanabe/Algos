aeec3e37298b7cd3fd496cfdc0946a80f37aacfc
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-13 23:14:48 -0500

    Breakdown Added

---
+++ StringCompression.js
@@ -0,0 +1,18 @@
+/**
+ * Variables:
+ *      s - holds the compression in string format
+ *      count - frequency of current character being tracked
+ *      prev - previous character seen, used for comparison in conjunction with count
+ * Loop through input array starting with the second character
+ *      If current character is equal to previous character
+ *          - increment count
+ *      Else:
+ *          - append to s the previous character and count (if count is greater than 1)
+ *          - set count back to 1
+ *      Set prev to equal current character
+ * Loop through each character in s in order to modify input array
+ *      Set input array at index i to equal s at index i
+ * Loop while input array's length is greater than s's length
+ *      Pop from input array
+ *
+ */
