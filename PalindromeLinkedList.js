8531a7cbcb007b20436124ded15ba230a2608041
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-13 19:44:08 -0500

    Solution Added

--- PalindromeLinkedList.js
+++ PalindromeLinkedList.js
@@ -26,3 +26,51 @@
  *      - Compare the values of list1 and list2 and if they are not equal, return false
  *  Return true
  */
+
+const isPalindrome = (head) => {
+  let numberOfNodes = 0;
+  let p1 = head;
+  while (p1) {
+    numberOfNodes++;
+    p1 = p1.next;
+  }
+  p1 = head;
+  let p2 = head;
+  let i = 0;
+  //if even number of nodes
+  if (numberOfNodes % 2 === 0) {
+    while (i < numberOfNodes / 2) {
+      p2 = p2.next;
+      i++;
+    }
+  } else {
+    while (i < Math.ceil(numberOfNodes / 2)) {
+      p2 = p2.next;
+      i++;
+    }
+  }
+
+  p2 = reverse(p2);
+  return compare(p1, p2);
+};
+
+const reverse = (list) => {
+  let curr = list;
+  let prev = null;
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+};
+
+const compare = (p1, p2) => {
+  while (p1 && p2) {
+    if (p1.val !== p2.val) return false;
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  return true;
+};
