53a65d439b98f57aedbe7b9a19e2f9284cca0e58
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-19 15:44:20 -0500

    Solution Added

--- DesignALeaderboard.js
+++ DesignALeaderboard.js
@@ -12,3 +12,24 @@
  * Reset: id
  *  If player id exists in hash table, set its score to zero
  */
+
+class Leaderboard {
+  constructor() {
+    this.scores = {};
+  }
+
+  addScore(id, score) {
+    this.scores[id] ? (this.scores[id] += score) : (this.scores[id] = score);
+  }
+
+  top(k) {
+    return Object.values(this.scores)
+      .sort((a, b) => b - a)
+      .slice(0, k)
+      .reduce((sum, num) => sum + num, 0);
+  }
+
+  reset(id) {
+    if (this.scores[id]) this.scores[id] = 0;
+  }
+}
