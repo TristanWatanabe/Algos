8fce03645b752e0434a1fbe11293fd40bd6bb71a
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-18 21:19:01 -0500

    Breakdown Added

--- MaximumLengthOfAConcatenatedStringWithUniqueValues.js
+++ MaximumLengthOfAConcatenatedStringWithUniqueValues.js
@@ -1,3 +1,26 @@
+/**
+ * Main: arr
+ *  Variables:
+ *      max - holds max number of characters that are unique
+ *  Call traverse function starting at zero while also passing arr and an empty string
+ *  Retun max
+ *
+ * Traverse: index, arr, currentString
+ *  Base Case: if index is higher than arr.length OR currentString is NOT unique, just return and do nothing
+ *  Recursive Case:
+ *      -have max equal to whichever is greater max or currentString.length
+ *      -loop from start to end of array
+ *          - push arr[i] to currentString array
+ *          - call traverse with i+1 and currentString
+ *          - pop from currentString
+ * IsUnique: string
+ *  Have hash table to store characters
+ *  Loop through each character in string
+ *     - if character is in hash already, return false
+ *     - else add character to hash
+ *   Return True
+ */
+
 const maxLength = (arr) => {
   let max = 0;
 
