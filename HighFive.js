fd54bc7d2215c320ba929de254f7e84c53cc77c5
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-20 19:13:47 -0500

    Solution Added

--- HighFive.js
+++ HighFive.js
@@ -6,10 +6,30 @@
  *      - if id already exists in scores hash table, push score to it
  *      - else, hash scores hash at key id to equal an array with the first score as its first element
  *  Loop through each key in scores
- *      - have variable s to hold the value at scores[key]
- *      - if the length of s is greater than 5
- *          - sort s in descending order and have s equal to just the first 5 elements of the sorted s array
+ *      - have variable studentScores to hold the value at scores[key]
+ *      - if the length of studentScores is greater than 5
+ *          - sort studentScores in descending order and have s equal to just the first 5 elements of the sorted studentScores array
  *      - calculate average for student
  *      - push to results [id, average]
  *  Return result
  */
+
+const highFive = (items) => {
+  let scores = {};
+  let result = [];
+  for (const item of items) {
+    const id = item[0],
+      score = item[1];
+    scores[id] ? scores[id].push(score) : (scores[id] = [score]);
+  }
+  for (const id in scores) {
+    let studentScores = scores[id];
+    if (studentScores.length > 5)
+      studentScores = studentScores.sort((a, b) => b - a).slice(0, 5);
+    const average = Math.floor(
+      studentScores.reduce((sum, num) => sum + num, 0) / 5
+    );
+    result.push([id, average]);
+  }
+  return result;
+};
