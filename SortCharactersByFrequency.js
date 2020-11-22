c0b0b68c01f55e221b442167ba7bbee41e003425
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-22 16:13:22 -0500

    Solution Added

--- SortCharactersByFrequency.js
+++ SortCharactersByFrequency.js
@@ -8,3 +8,27 @@
  * Loop through each bucket starting from the end (so it's descending order) and add the characters onto result array
  * Return a stringified version of result by join all characters together
  */
+
+const frequencySort = (s) => {
+  let hash = {};
+  let bucket = new Array(s.length).fill(null);
+  let result = [];
+  //build hash table
+  for (const char of s) {
+    hash[char] ? hash[char]++ : (hash[char] = 1);
+  }
+  //place characters in correct bucket
+  for (const key in hash) {
+    const freq = hash[key];
+    bucket[freq] ? bucket[freq].push(key) : (bucket[freq] = [key]);
+  }
+  //build solution string based on bucket order
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i] !== null) {
+      for (const char of bucket[i]) {
+        result.push(char.repeat(i));
+      }
+    }
+  }
+  return result.join("");
+};
