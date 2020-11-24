f7ecec1e1e84eebe026c0eb2be2bc7455dbea3ca
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-23 20:17:58 -0500

    Breakdown Added

--- KillProcesses.js
+++ KillProcesses.js
@@ -1,3 +1,17 @@
+/**
+ * Variables:
+ *      hash: hash table with key value pair of parent-children processes
+ *      result: array to hold process id's that are killed
+ *      processesToKill: stack to hold the process that need to be killed
+ * Loop through from index 0 to ppid length
+ *      - add parent key and it's children to hash
+ * Loop through while there's still elements in processesToKill stack
+ *      - pop from processesToKill stack
+ *      - if the current process that was popped is a parent, push its children to processesToKill stack
+ *      - push current process to result
+ * Return result
+ */
+
 const killProcess = (pid, ppid, kill) => {
   let hash = {};
   let result = [];
