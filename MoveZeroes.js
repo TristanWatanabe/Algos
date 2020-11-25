8f6c21b4f7fbc9c6fad033747d7989d9069a5018
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-24 19:52:55 -0500

    Breakdown Added

--- MoveZeroes.js
+++ MoveZeroes.js
@@ -1,3 +1,13 @@
+/**
+ * Variable:
+ *      index: keeps track of which index in input array will be modified with a non zero available
+ * Loop through array
+ *      if current number is not zero
+ *          - set array at index to equal this number
+ *          - increment index
+ * Loop from index to array length and set the remaining elements to zero
+ */
+
 const moveZeroes = (nums) => {
   let index = 0;
   for (let i = 0; i < nums.length; i++) {
