5a74ed00d8a0f57870fa5d52ae9e9ef540ebab3d
Tristan Watanabe <tristan.watanabe@gmail.com>
2020-11-26 20:46:45 -0500

    Solution Added

--- SquaresOfASortedArray.js
+++ SquaresOfASortedArray.js
@@ -10,5 +10,26 @@
  *      else
  *          - push value at pointer right
  *          - decrement right
+ * Reverse result since this is currently sorted in descending order
  * Return result
  */
+
+const sortedSquares = (A) => {
+  let result = [];
+  let left = 0,
+    right = A.length - 1;
+  while (left <= right) {
+    const val1 = Math.abs(A[left]);
+    const val2 = Math.abs(A[right]);
+    //we look for the greater value because we know for sure that the last element in input array is the highest
+    if (val1 >= val2) {
+      result.push(Math.pow(val1, 2));
+      left++;
+    } else {
+      result.push(Math.pow(val2, 2));
+      right--;
+    }
+  }
+  //reverse result since it's currently sorted in descending order
+  return result.reverse();
+};
