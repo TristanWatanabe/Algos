d4df8257b3cf1af8952a5cedf43096a2306b19a2
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-02 19:59:37 -0500

    Breakdown Added

---
+++ ValidPalindrome.js
@@ -0,0 +1,10 @@
+/**
+ * Variables:
+ *  low - low pointer starting at the first character of s
+ *  high - high pointer starting at the last character of s
+ * Remove characters in s that are not a to z or 0 to 9
+ * Loop while low is less than or equal to high
+ *  -if character at pointer low is not equal to character at pointer high, return false
+ *  -increment low pointer and decrement high pointer
+ *
+ */
