4d37e21c3f8a2c18954ff375dd8488771fdbde03
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-02 20:00:37 -0500

    Solution Added

--- ValidPalindrome.js
+++ ValidPalindrome.js
@@ -6,5 +6,21 @@
  * Loop while low is less than or equal to high
  *  -if character at pointer low is not equal to character at pointer high, return false
  *  -increment low pointer and decrement high pointer
- *
+ * Since we never returned false, return true
  */
+
+const isPalindrome = (s) => {
+  const reg = /[a-z0-9]/i;
+  s = s
+    .split("")
+    .filter((char) => char.match(reg))
+    .join("")
+    .toLowerCase();
+  let low = 0,
+    high = s.length - 1;
+  while (low <= high) {
+    if (s[low] !== s[high]) return false;
+    low++, high--;
+  }
+  return true;
+};
