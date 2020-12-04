bf7f26b5a69a4ee0993a0696c9459908d8de8d82
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-04 18:48:39 -0500

    Solution Added

--- LogRateLimiter.js
+++ LogRateLimiter.js
@@ -7,3 +7,15 @@
  *      - else update/add message to logs hash table with timestamp
  *      - return true
  */
+
+class Logger {
+  constructor() {
+    this.logs = {};
+  }
+  shouldPrintMessage(timestamp, message) {
+    if (this.logs[message] !== undefined && timestamp - this.logs[message] < 10)
+      return false;
+    this.logs[message] = timestamp;
+    return true;
+  }
+}
