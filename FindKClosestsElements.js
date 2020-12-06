209ef3122ae626dfcb7cf4c6134078f1d319c67a
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-06 18:23:55 -0500

    Solution Added

--- FindKClosestsElements.js
+++ FindKClosestsElements.js
@@ -12,3 +12,19 @@
  *      - move right pointer to equal mid
  * Return array sublist from left to left + k
  */
+
+const findClosestElements = (arr, k, x) => {
+  if (x < arr[0]) return arr.slice(0, k);
+  if (x > arr[arr.length - 1]) return arr.slice(-k);
+  let left = 0;
+  let right = arr.length - k;
+  while (left < right) {
+    let mid = Math.floor((right - left) / 2 + left);
+    if (x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return arr.slice(left, left + k);
+};
