5c770783447946bdc61f35ee6d1049cff305729f
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-09 20:07:01 -0500

    Breakdown Added

---
+++ FizzBuzz.js
@@ -0,0 +1,10 @@
+/**
+ * Variables:
+ *  result: array that holds each iteration's result
+ * Loop from 1 to N
+ *  - create a variable str initialized as an empty string
+ *  - if i is a multiple of 3, append "Fizz" to str
+ *  - if i is a multiple of 5, append "Buzz" to str
+ *  - push str to result if its not empty, else push i to result
+ * Return result
+ */
