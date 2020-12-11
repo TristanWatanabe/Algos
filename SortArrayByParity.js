10a8eef21393c0245eb0f7f55b56ccb02d15ef14
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-10 22:18:04 -0500

    Breakdown Added

---
+++ SortArrayByParity.js
@@ -0,0 +1,9 @@
+/**
+ * Variables:
+ *  odds - array of odd numbers
+ *  evens - array of even numbers
+ * Loop through each number in array
+ *  - if number is even, push to evens array
+ *  - else, push to odds array
+ * Return a concatenated version of evens and odds with evens being first
+ */
