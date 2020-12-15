8c99a0ab1170c9cf57728b484bfd33318772d260
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-14 23:10:08 -0500

    Solution Added

--- Sqrtx.js
+++ Sqrtx.js
@@ -11,3 +11,18 @@
  *  - else: return pivot since that's the correct square root
  * Return right
  */
+
+const mySqrt = (x) => {
+  if (x < 2) return x;
+
+  let left = 2,
+    right = Math.floor(x / 2);
+  while (left <= right) {
+    const pivot = Math.floor(left + (right - left) / 2);
+    const num = pivot * pivot;
+    if (num > x) right = pivot - 1;
+    else if (num < x) left = pivot + 1;
+    else return pivot;
+  }
+  return right;
+};
