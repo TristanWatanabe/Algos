f91fe6ec53234af64f479629f397d4590886d6e4
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-17 23:01:35 -0500

    Breakdown Added

---
+++ DesignHitCounter.js
@@ -0,0 +1,12 @@
+/*
+    contructor:    
+        have a dictionary that stores timestamp: hits
+    
+    hits
+        add to dictionary if timestamp doesnt exist, if it does, increment count
+        
+    getHits
+        convert the dictionary into an array
+        return total hits for every element that's within the range of timestamp-300 and timestamp
+
+*/
