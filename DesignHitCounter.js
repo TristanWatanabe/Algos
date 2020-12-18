066db48b4e71189798bfb39027f9cb169a490631
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-17 23:02:27 -0500

    Solution Added

--- DesignHitCounter.js
+++ DesignHitCounter.js
@@ -10,3 +10,23 @@
         return total hits for every element that's within the range of timestamp-300 and timestamp
 
 */
+
+class HitCounter {
+  constructor() {
+    this.timestamps = {};
+  }
+
+  hit(timestamp) {
+    this.timestamps[timestamp]
+      ? this.timestamps[timestamp]++
+      : (this.timestamps[timestamp] = 1);
+  }
+
+  getHits(timestamp) {
+    let min = timestamp - 300 > 0 ? timestamp - 300 : 0;
+    let arr = Object.entries(this.timestamps).filter(
+      (hit) => Number(hit[0]) > min && Number(hit[0]) <= timestamp
+    );
+    return arr.reduce((sum, hit) => sum + hit[1], 0);
+  }
+}
