b3c0b9d9e1778247f358a03c9439265d657aedf7
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-20 18:37:44 -0500

    Breakdown Added

---
+++ AddDigits.js
@@ -0,0 +1,6 @@
+/**
+ * Find the digital root of the input number and return it
+ *  - if the number is equal 0, then 0 is the digital root
+ *  - if the number divided by 9 has a remainder of zero, then 9 is the digital root
+ *  - else: the digital root will be the remainder of the number divided by 9
+ */
