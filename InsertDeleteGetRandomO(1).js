8540721c721c9e10a95888856c68abba4aa934fc
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-19 22:42:45 -0500

    Solution Added

--- InsertDeleteGetRandomO(1).js
+++ InsertDeleteGetRandomO(1).js
@@ -21,3 +21,52 @@
  *  -Generate random index using Math.random
  *  -return value at nums at random generated index
  */
+
+class RandomizedSet {
+  constructor() {
+    this.set = {};
+    this.nums = [];
+  }
+  /**
+   * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+   * @param {number} val
+   * @return {boolean}
+   */
+  insert(val) {
+    if (this.set[val] === undefined) {
+      this.set[val] = this.nums.length;
+      this.nums.push(val);
+      return true;
+    }
+    return false;
+  }
+  /**
+   * Removes a value from the set. Returns true if the set contained the specified element.
+   * @param {number} val
+   * @return {boolean}
+   */
+  remove(val) {
+    if (this.set[val] >= 0) {
+      let index = this.set[val];
+      //swap from nums[index] to nums[nums.length - 1]
+      let last = this.nums[this.nums.length - 1];
+      this.nums[index] = last;
+      //update the index in dictionary to reflect change
+      this.set[last] = index;
+      this.nums.pop();
+      delete this.set[val];
+      return true;
+    }
+    return false;
+  }
+  /**
+   * Get a random element from the set.
+   * @return {number}
+   */
+  getRandom() {
+    let max = this.nums.length;
+    let min = 0;
+    let index = Math.floor(Math.random() * max + min);
+    return this.nums[index];
+  }
+}
