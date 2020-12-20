61fd1bb336344f9f658a51cc41f8b7020334656f
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-20 18:38:30 -0500

    Solution Added

--- AddDigits.js
+++ AddDigits.js
@@ -4,3 +4,9 @@
  *  - if the number divided by 9 has a remainder of zero, then 9 is the digital root
  *  - else: the digital root will be the remainder of the number divided by 9
  */
+
+const addDigits = (num) => {
+  if (num === 0) return 0;
+  if (num % 9 === 0) return 9;
+  return num % 9;
+};
