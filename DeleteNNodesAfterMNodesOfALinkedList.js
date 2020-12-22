79e4dbf1505020adba532178a81040f48b73a114
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-21 23:18:00 -0500

    Breakdown Added

---
+++ DeleteNNodesAfterMNodesOfALinkedList.js
@@ -0,0 +1,15 @@
+/*
+    Main:
+    Have a count variable equal to 1 at first because we're pointing at the head already
+    While loop while curr is not null
+        If count equals to m
+            get the next node to connect this node with
+            reset count back to equal 1
+        else 
+            increment count
+        Move pointer forward
+    Return head
+            
+    GetNextNode helper function: n, node
+        - return a node that's n distance away from current node passed
+*/
