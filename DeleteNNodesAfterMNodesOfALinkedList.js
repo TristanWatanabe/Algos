c9805fa98576350d1a7154c09906bc03a3ad01a6
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-21 23:18:34 -0500

    Solution Added

--- DeleteNNodesAfterMNodesOfALinkedList.js
+++ DeleteNNodesAfterMNodesOfALinkedList.js
@@ -13,3 +13,27 @@
     GetNextNode helper function: n, node
         - return a node that's n distance away from current node passed
 */
+
+const deleteNodes = (head, m, n) => {
+  let count = 1;
+  let curr = head;
+  while (curr) {
+    if (count === m) {
+      curr.next = getNext(n, curr);
+      count = 1;
+    } else {
+      count++;
+    }
+    curr = curr.next;
+  }
+  return head;
+};
+
+const getNext = (n, curr) => {
+  for (let i = 0; i <= n; i++) {
+    if (!curr) break;
+    curr = curr.next;
+  }
+
+  return curr;
+};
