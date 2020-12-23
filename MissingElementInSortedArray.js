59cb37a02c50308627a49fc821e8b957dd27081c
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-22 23:12:09 -0500

    Breakdown Added

---
+++ MissingElementInSortedArray.js
@@ -0,0 +1,9 @@
+/*  
+    Edge Case: if kth missing is greater than last element in array
+    
+    Loop while number of missing is less than k, increment index
+    return missing number that's between index-1 and index 
+    
+    Missing helper function: return how many numbers are missing until nums[index]
+    
+*/
