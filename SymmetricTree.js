cf8a2f79ada04d6d5f4a20506ad6cb093c2709ac
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-23 23:04:22 -0500

    Space And Time Complexity Added

--- SymmetricTree.js
+++ SymmetricTree.js
@@ -8,3 +8,9 @@ const compare = (node1, node2) => {
   if (!node1 || !node2 || node1.val !== node2.val) return false;
   return compare(node1.left, node2.right) && compare(node1.right, node2.left);
 };
+
+/**
+ * N - number of nodes in tree
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
