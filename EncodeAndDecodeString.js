9d19987279c3414b9e4c0e4b5054ca2a7e1d9087
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-24 23:02:55 -0500

    Breakdown Added

---
+++ EncodeAndDecodeString.js
@@ -0,0 +1,14 @@
+/**
+  
+  Encode:
+    Input type is an array
+    Since the input is a string from ASCII characters 0-256, then we can use ascii characters outside that range as our way of encoding the string
+    Edge case: if string is empty, use charcode 257 to encode it
+    else: use charcode 258 to encode the string and join the words together with that charcode
+
+  Decode:
+    Input is a string
+    Edge case: if str is equal to charcode 257, return empty string
+    else: return a split of the string removing the charcode 258 encoder
+
+*/
