5b0f0f5cc9c503827244664383ba397d7f86bafb
TristanWatanabe <tristan.watanabe@gmail.com>
2020-12-27 00:06:52 -0500

    Solution Added

--- MergeIntervals.js
+++ MergeIntervals.js
@@ -11,3 +11,23 @@
     Push the last min and max to the results array
     Return results
 */
+
+const merge = (intervals) => {
+  if (!intervals.length) return [];
+  intervals.sort((a, b) => a[0] - b[0]);
+  let min = intervals[0][0];
+  let max = intervals[0][1];
+  let result = [];
+  for (let i = 1; i < intervals.length; i++) {
+    let interval = intervals[i];
+    if (interval[0] <= max) {
+      max = Math.max(max, interval[1]);
+    } else {
+      result.push([min, max]);
+      min = interval[0];
+      max = interval[1];
+    }
+  }
+  result.push([min, max]);
+  return result;
+};
