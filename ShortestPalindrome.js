a9e4c891d231e4c50030ae4ea7b5e0509cb6c6a6
TristanWatanabe <tristan.watanabe@gmail.com>
2021-01-01 21:24:18 -0500

    Breakdown Added

---
+++ ShortestPalindrome.js
@@ -0,0 +1,8 @@
+/*
+    Have a reversed version of the string as a variable
+    Loop through S and compare the current whole string with the reversed string
+        if substring of S is equal to substring of reversed (means we've found the shortest palindrome)
+            -append  reverse substring from zero to i to S and then return
+    Return empty string for edge case where S is an empty string
+
+*/
