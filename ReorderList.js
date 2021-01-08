a5669b9cddb792d780800779982dd0b477ad83af
TristanWatanabe <tristan.watanabe@gmail.com>
2021-01-07 23:39:18 -0500

    Breakdown Added

--- ReorderList.js
+++ ReorderList.js
@@ -1,3 +1,21 @@
+/**
+ * Have a stack to hold each node in the list
+ * Set each next pointer for a node to null
+ * Initialize pointers low and high to 1 and stack length -1 respectively
+ * Have variables curr set to head and i set to 0
+ * Loop while i is less than number of nodes in stack
+ *  - If i is even,
+ *      - set curr's next pointer to point to stack at index high
+ *      - decrement high
+ *  - else:
+ *      - set curr's next pointer to point to stack at index low
+ *      - increment low
+ *  - move curr forward
+ *  - increment i
+ * Return head
+ *
+ */
+
 const reorderList = (head) => {
   let stack = [];
   let curr = head;
